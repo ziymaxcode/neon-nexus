8a216e2df521d9224fbaa198cb2f4b5924f824f7
ZIYAN211 <nnm22cs211@nmamit.in>
2026-03-30 10:37:51 +0530

    lat build

--- electron/main.cjs
+++ electron/main.cjs
@@ -5,6 +5,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+     icon: path.join(__dirname, 'assets/nlogo.ico'),
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
